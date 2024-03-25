8294e161ae26a56bf47d1ae3cbf65a473a5bcf84
Tristan Davis <Tristandavis888@gmail.com>
2024-03-24 17:31:15 -0700

    audit always in order

--- audit/app.py
+++ audit/app.py
@@ -15,29 +15,24 @@ KAFKA_PORT = CONFIG['KAFKA_PORT']
 KAFKA_TOPIC = CONFIG['KAFKA_TOPIC']
 KAFKA_TRIES = CONFIG['KAFKA_TRIES']
 KAFKA_DELAY = CONFIG['KAFKA_DELAY']
-consumer = None
+kafka = None
 
 
 def kafka_init():
-    global consumer
+    global kafka
     kafka = Kafka(KAFKA_HOST, KAFKA_PORT, LOGGER, KAFKA_TRIES, KAFKA_DELAY)
-
-    consumer = kafka.get_consumer(KAFKA_TOPIC)
-    if not consumer:
-        LOGGER.error("Failed to get consumer")
-        exit(1)
 
 
 kafka_init()
 
 
 def get_event_at_index(event, index):
-    global consumer
+    global kafka
     LOGGER.info(f"Retrieving {event} at index: {index} ")
+    consumer = kafka.get_consumer(KAFKA_TOPIC)
     try:
         curr = 0
         for msg in consumer:
-            print("message: ", msg)
             msg_str = msg.value.decode('utf-8')
             msg = json.loads(msg_str)
             if msg['type'] == event:
