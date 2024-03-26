328b5a45fe25f75766f32a904c4e0a605ec66fb7
Tristan Davis <Tristandavis888@gmail.com>
2024-03-25 23:09:11 -0700

    added all no sec

--- receiver/app.py
+++ receiver/app.py
@@ -96,4 +96,4 @@ app.add_api("./openapi.yaml", strict_validation=True, validate_responses=True)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8080)  # nosec
