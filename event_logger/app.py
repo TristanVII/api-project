76e980dc3b979aec21dca31e384adde10359241f
Tristan Davis <Tristandavis888@gmail.com>
2024-04-04 14:33:05 -0700

    msg

--- event_logger/app.py
+++ event_logger/app.py
@@ -90,7 +90,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-app.add_api("./openapi.yaml", strict_validation=True, validate_responses=True)
+app.add_api("./openapi.yaml", strict_validation=True,
+            validate_responses=True, base_path="/event_logger")
 
 
 if __name__ == "__main__":
