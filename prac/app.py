24fce1b291fadd74245d6de1677ee9d3eaae5d10
Tristan Davis <Tristandavis888@gmail.com>
2024-04-17 22:31:26 -0700

    added mock

---
+++ prac/app.py
@@ -0,0 +1,39 @@
+import time
+import connexion
+import requests
+import datetime
+import json
+from connexion.middleware import MiddlewarePosition
+from starlette.middleware.cors import CORSMiddleware
+from connexion import FlaskApp, NoContent
+from load_configs import load_app_conf, load_log_conf
+
+LOGGER = load_log_conf()
+CONFIG = load_app_conf()
+
+
+def mock_get(idx):
+    return {"response": idx}, 201
+
+
+def mock_post(body):
+    LOGGER.info(f"Received {body}")
+    return NoContent, 201
+
+
+app = FlaskApp(__name__, specification_dir='')
+
+app.add_middleware(
+    CORSMiddleware,
+    position=MiddlewarePosition.BEFORE_EXCEPTION,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.add_api("./openapi.yaml", strict_validation=True,
+            validate_responses=True, base_path="/mock")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8999)
---
+++ prac/load_configs.py
@@ -0,0 +1,65 @@
+import logging
+import logging.config
+import yaml
+import os
+
+
+def get_app_conf_file():
+    app_conf_file = ''
+    if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "TEST":
+        print("In Test Env")
+        app_conf_file = "/config/app_conf.yml"
+    else:
+        print("In Dev Env")
+        app_conf_file = "app_conf.yml"
+    return app_conf_file
+
+
+def get_log_conf_file():
+    log_conf_file = ''
+    if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "TEST":
+        print("In Test Env")
+        log_conf_file = "/config/log_conf.yml"
+    else:
+        print("In Dev Env")
+        log_conf_file = "log_conf.yml"
+    return log_conf_file
+
+
+def load_log_conf():
+    """
+    Returns:
+        logger: Logger
+    """
+    with open(get_log_conf_file(), 'r') as f:
+        log_config = yaml.safe_load(f.read())
+        logging.config.dictConfig(log_config)
+        logger = logging.getLogger('basicLogger')
+
+    return logger
+
+
+# TODO: Change
+def load_app_conf():
+    """
+    Returns:
+      Dictionary containing:
+      KAFKA_SERVER: string
+      KAFKA_PORT: string
+      KAFKA_TOPIC: string
+    """
+    with open(get_app_conf_file(), 'r') as f:
+        app_config = yaml.safe_load(f.read())
+
+        # kafka = app_config['events']
+        # datastore = app_config['datastore']
+        # config_dict = {
+        #     'DATABASE': datastore['filename'],
+        #     'KAFKA_SERVER': kafka['hostname'],
+        #     'KAFKA_PORT': kafka['port'],
+        #     'KAFKA_EVENT_LOG': kafka['event_log'],
+        #     'KAFKA_TRIES': kafka['max_tries'],
+        #     'KAFKA_DELAY': kafka['delay']
+        # }
+    return {}
+    return config_dict
